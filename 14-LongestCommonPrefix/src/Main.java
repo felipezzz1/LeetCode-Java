c7ca4199d8fe79c93d42f95dec4899117948a2fa
felipezzz1 <felipezorzo78@gmail.com>
2025-08-27 23:09:54 -0300

    14 - Longest Common Prefix

---
+++ 14-LongestCommonPrefix/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.longestCommonPrefix(new String[]{"flower","flow","flight"}));
+    }
+}
