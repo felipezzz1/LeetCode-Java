cbcad31dd9d52a2cbdcda7b89113be41354e34ef
felipezzz1 <felipezorzo78@gmail.com>
2026-01-18 13:58:17 -0300

    implementation of problem 704, same as I did in binary search implementation

---
+++ 704-BinarySearch/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+            Solution solution = new Solution();
+
+            System.out.println(solution.search(new int[]{-1,0,3,5,9,12}, 9));
+
+            System.out.println(solution.search(new int[]{-1,0,3,5,9,12}, 2));
+    }
+}
