e8de48f7983249640018be53283b65372e03ede8
felipezzz1 <felipezorzo78@gmail.com>
2026-01-23 13:37:52 -0300

    implementation of 21-mergeTwoLinkedLists leetcode

---
+++ 21-MergeTwoLinkedLists/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        solution.mergeTwoLists(list1, list2);
+    }
+}
