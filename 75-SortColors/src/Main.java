e991d4ff106a6d1d8c8e2a834943e4eddc4802d8
felipezzz1 <felipezorzo78@gmail.com>
2026-01-24 20:54:44 -0300

    leetcode 75 SortColors implemenation

---
+++ 75-SortColors/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] arr = new int[]{0,1,5,4,8,6};
+
+        solution.quickSort(arr, 0, arr.length-1);
+
+    }
+}
