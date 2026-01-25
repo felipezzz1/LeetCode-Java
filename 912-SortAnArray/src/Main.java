0cbbd63b11757902753811447c492da3f6481476
felipezzz1 <felipezorzo78@gmail.com>
2026-01-24 21:20:36 -0300

    leectode 912 SortAnArray implementation

--- 912-SortAnArray/src/Main.java
+++ 912-SortAnArray/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Oi");
+        Solution solution = new Solution();
+
+        int[] arr = new int[]{5,2,3,1};
+
+        int[] arr1 = new int[]{5,1,1,2,0,0};
+
+        System.out.println(Arrays.toString(solution.sortArray(arr)));
+        System.out.println(Arrays.toString(solution.sortArray(arr1)));
+
+
     }
 }
--- 912-SortAnArray/src/Solution.java
+++ 912-SortAnArray/src/Solution.java
@@ -1,6 +1,34 @@
 public class Solution {
     public int[] sortArray(int[] nums) {
-        System.out.println("Oi");
+        quickSort(nums, 0, nums.length-1);
+
         return nums;
+    }
+
+    public void quickSort(int[] nums, int low, int high){
+        if(low < high) {
+            int pi = partition(nums, low, high);
+            quickSort(nums, low, pi-1);
+            quickSort(nums, pi+1, high);
+        }
+    }
+
+    public int partition(int[] arr, int low, int high){
+        int pivot = arr[high];
+        int i = low-1;
+
+        for (int j = low; j < high; j++) {
+            if(arr[j] <= pivot){
+                i++;
+                int aux = arr[i];
+                arr[i] = arr[j];
+                arr[j] = aux;
+            }
+        }
+        int aux = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = aux;
+
+        return i+1;
     }
 }
