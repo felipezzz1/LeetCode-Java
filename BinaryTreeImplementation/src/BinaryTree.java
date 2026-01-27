a01062c83fd4bb3635731411d4b2f31ece51e744
felipezzz1 <felipezorzo78@gmail.com>
2026-01-27 10:57:20 -0300

    binaryTree traversals implementation

--- BinaryTreeImplementation/src/BinaryTree.java
+++ BinaryTreeImplementation/src/BinaryTree.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinaryTree {
     TreeNode root;
 
@@ -41,5 +44,47 @@ public class BinaryTree {
         if (val < node.val) return searchRecursive(node.left, val);
 
         return searchRecursive(node.right, val);
+    }
+
+    public List<Integer> preorderTraversal(){
+        List<Integer> result = new ArrayList<>();
+        preorderTraversalRecursive(root, result);
+        return result;
+    }
+
+    private void preorderTraversalRecursive(TreeNode node, List<Integer> result){
+        if(node != null){
+            result.add(node.val);
+            preorderTraversalRecursive(node.left, result);
+            preorderTraversalRecursive(node.right, result);
+        }
+    }
+
+    public List<Integer> inorderTraversal(){
+        List<Integer> result = new ArrayList<>();
+        inorderTraversalRecursive(root, result);
+        return result;
+    }
+
+    private void inorderTraversalRecursive(TreeNode node, List<Integer> result){
+        if(node != null){
+            inorderTraversalRecursive(node.left, result);
+            result.add(node.val);
+            inorderTraversalRecursive(node.right, result);
+        }
+    }
+
+    public List<Integer> postorderTraversal(){
+        List<Integer> result = new ArrayList<>();
+        postorderTraversalRecursive(root, result);
+        return result;
+    }
+
+    private void postorderTraversalRecursive(TreeNode node, List<Integer> result){
+        if(node != null){
+            postorderTraversalRecursive(node.left, result);
+            postorderTraversalRecursive(node.right, result);
+            result.add(node.val);
+        }
     }
 }
--- BinaryTreeImplementation/src/Main.java
+++ BinaryTreeImplementation/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
 
-        int[] valuesToInsert = {10,5,15,3,7,12,18};
+        int[] valuesToInsert = {5,3,1,10,15,7};
 
         for (int val : valuesToInsert) {
             tree.insert(val);
@@ -10,7 +10,11 @@ public class Main {
 
         System.out.println(tree.search(7));
         System.out.println(tree.search(14));
-        System.out.println(tree.search(21));
+        System.out.println(tree.search(10));
         System.out.println(tree.search(18));
+
+        System.out.println("Preorder Traversal: " + tree.preorderTraversal());
+        System.out.println("Inorder Traversal: " + tree.inorderTraversal());
+        System.out.println("Postorder Traversal: " + tree.postorderTraversal());
     }
 }
